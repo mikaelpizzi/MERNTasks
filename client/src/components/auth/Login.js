9f437d192b0d2541e8b13dd0aa66f62bef621495
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-18 20:51:57 -0400

    [ADD] Add form input data to state

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import { Link }from 'react-router-dom';
+
 const Login = () => {
 
-    const onChangeSignIn = () => {
+    
+    // Sign in state
+    const [ user, saveUser ] = useState({
+        email: '',
+        password: ''
+    });
+    
+    // Extract from user
+    const { email, password } = user;
 
+
+    const onChangeSignIn = e => {
+        saveUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    // When user wants to sign in
+    const onSubmitSignIn = e => {
+        e.preventDefault();
+
+        // Validate that there are no empty field
+
+        // Pass to action in reducer
     }
 
     return (  
@@ -9,7 +35,9 @@ const Login = () => {
             <div className="contenedor-form sombra-dark">
                 <h1>Sign in</h1>
 
-                <form>
+                <form
+                    onSubmit={onSubmitSignIn}
+                >
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
                         <input  
@@ -17,6 +45,7 @@ const Login = () => {
                             id="email"
                             name="email"
                             placeholder="Your Email"
+                            value={email}
                             onChange={onChangeSignIn}
                         />
                     </div>
@@ -27,6 +56,7 @@ const Login = () => {
                             id="password"
                             name="password"
                             placeholder="Your Password"
+                            value={password}
                             onChange={onChangeSignIn}
                         />
                     </div>
@@ -39,6 +69,10 @@ const Login = () => {
                         />
                     </div>
                 </form>
+
+                <Link to={'/new-account'} className="enlace-cuenta">
+                    New Account
+                </Link>
             </div>
         </div>
     );
