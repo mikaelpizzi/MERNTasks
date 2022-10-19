8e3c071a0a734c6eab831c28c9a91cb726ee59fd
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-18 20:26:52 -0400

    [FEAT] Create login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,46 @@
 const Login = () => {
+
+    const onChangeSignIn = () => {
+
+    }
+
     return (  
-        <h1>From Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Sign in</h1>
+
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input  
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Your Email"
+                            onChange={onChangeSignIn}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input  
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Your Password"
+                            onChange={onChangeSignIn}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Sign in"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
  
