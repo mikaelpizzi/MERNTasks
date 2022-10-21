224841faba52f909f6917782c6f0335637fdc22f
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-20 22:11:43 -0400

    [FEAT] Configure user routes

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ connectDB();
 // App port
 const PORT = process.env.PORT || 4000;
 
+// Import routes
+app.use('/api/users', require('./routes/users'));
+
 // Start app
 app.listen(PORT, () => {
     console.log(`The server is running in port ${PORT}`);
---
+++ server/routes/users.js
@@ -0,0 +1,11 @@
+// Routes for creating user
+const express = require('express');
+const router = express.Router();
+
+// Create a user
+// /api/users
+router.post('/', () => {
+
+});
+
+module.exports = router;
