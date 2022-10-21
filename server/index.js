a6f74fbba6d45b9177b9ddc13677faedd1db0540
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-20 20:46:39 -0400

    [ADD] Create Express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log("From server! wuuu");
+const express = require('express');
+
+// Create server
+const app = express();
+
+// App port
+const PORT = process.env.PORT || 4000;
+
+// Define main page
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+// Start app
+app.listen(PORT, () => {
+    console.log(`The server is running in port ${PORT}`);
+})
